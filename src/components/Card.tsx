1b42ce07b6743e7b2c67f6c5c236961ad19cbb6e
Yoann Delion <yoanndelion@gmail.com>
2021-11-10 12:15:50 +0100

    test(Card)

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,32 +1,48 @@
 import styled from 'styled-components'
 import colors from '../utils/style/colors'
 import DefaultPicture from '../assets/profile.png'
+import { useState } from 'react'
+import { useTheme } from '../utils/hooks/useTheme'
+
+const CardLabel = styled.span`
+  color: ${({ theme }) => (theme === 'light' ? colors.primary : '#ffffff')};
+  font-size: 22px;
+  font-weight: normal;
+  padding-left: 15px;
+`
+
+const CardTitle = styled.div`
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
+  font-size: 22px;
+  font-weight: normal;
+  align-self: center;
+  height: 25px;
+  display: flex;
+  align-items: center;
+`
+
+const CardImg = styled.img`
+  height: 150px;
+  width: 150px;
+  align-self: center;
+  border-radius: 50%;
+`
 
 const CardContainer = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: space-around;
   padding: 15px;
-  background-color: ${colors.backgroundLight};
+  background-color: ${({ theme }) =>
+    theme === 'light' ? colors.backgroundLight : colors.backgroundDark};
   border-radius: 30px;
-  width: 350px;
-  transition: 200ms;
+  width: 300px;
+  height: 300px;
   &:hover {
     cursor: pointer;
-    box-shadow: 2px 2px 10px #e2e3e9;
   }
 `
 
-const CardLabel = styled.span`
-  color: ${colors.primary};
-  font-size: 22px;
-  font-weight: bold;
-`
-
-const CardImg = styled.img`
-  height: 80px;
-  width: 80px;
-  border-radius: 50%;
-`
 interface PropsTypes {
   label: string
   title: string
@@ -34,11 +50,17 @@ interface PropsTypes {
 }
 
 export default function Card({ label, title, picture = DefaultPicture }: PropsTypes) {
+  const { theme } = useTheme()
+  const [isFavorite, setIsFavorite] = useState(false)
+  const star = isFavorite ? '⭐' : ''
+
   return (
-    <CardContainer>
-      <CardLabel>{label}</CardLabel>
+    <CardContainer theme={theme} onClick={() => setIsFavorite(!isFavorite)}>
+      <CardLabel theme={theme}>{label}</CardLabel>
       <CardImg src={picture} alt='freelance' />
-      <span>{title}</span>
+      <CardTitle theme={theme}>
+        {star} {title} {star}
+      </CardTitle>
     </CardContainer>
   )
 }
