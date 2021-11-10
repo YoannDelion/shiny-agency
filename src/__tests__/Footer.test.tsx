4d82006658a81a25399852d43e449f01269c4931
Yoann Delion <yoanndelion@gmail.com>
2021-11-10 11:44:33 +0100

    test(Foooter) render and change theme

---
+++ src/__tests__/Footer.test.tsx
@@ -0,0 +1,17 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import Footer from '../components/Footer'
+import { ThemeProvider } from '../utils/context'
+
+describe('Footer component', () => {
+  it('should render without crash', async () => {
+    render(
+      <ThemeProvider>
+        <Footer />
+      </ThemeProvider>
+    )
+    const nightModeButton = screen.getByRole('buttontest')
+    expect(nightModeButton.textContent).toBe('Changer de mode : ☀️')
+    fireEvent.click(nightModeButton)
+    expect(nightModeButton.textContent).toBe('Changer de mode : 🌙')
+  })
+})
